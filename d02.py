f659f711e4398ba657612d0dfdc6c9a815149fcf
Andreas Aaberge Eide <andreasaaeide@gmail.com>
2020-12-03 16:10:55 +0100

    Day 2 v2: Fix stupid overkill solution

--- d02.py
+++ d02.py
@@ -1,4 +1,4 @@
-from typing import Tuple, List, Callable, Any
+from typing import Tuple, List, Callable
 from functools import reduce
 
 example = [
@@ -7,41 +7,11 @@ example = [
     '2-9 c: ccccccccc',
 ]
 
-
 ###################
 # HELPERS         #
 ###################
 
-
 ParsedLine = Tuple[Tuple[int, int], str, str]
-
-
-def indexed_reduce(
-    function: Callable[[Any, Any, int], Any],
-    sequence: List[Any],
-    initial: Any = None,
-    verbose: bool = False
-) -> Any:
-    """
-    A reducer which gives the reduce function index as well, which functools.reduce
-    annoyingly doesn't. Only accepts List as sequence.
-    """
-    if len(sequence) == 0:
-        raise TypeError(
-            "indexed_reduce() of empty iterable with no initial value")
-
-    if initial == None:
-        value = sequence[0]
-    else:
-        value = initial
-
-    for i in range(len(sequence)):
-        value = function(value, sequence[i], i)
-
-        if verbose:
-            print(f"{'='*20}\nsequence[{i}]: {sequence[i]}\nvalue: {value}")
-
-    return value
 
 
 def parse_line(line: str) -> ParsedLine:
@@ -52,7 +22,6 @@ def parse_line(line: str) -> ParsedLine:
     string = splt[2]
 
     return ((min, max), char, string)
-
 
 ########################
 # Part 1: Check count  #
@@ -69,29 +38,17 @@ def ct_reduce(char: str) -> Callable[[int, str], int]:
 def ct_check(parsed: ParsedLine) -> bool:
     ((min, max), char, string) = parsed
 
-    ct = reduce(ct_reduce(char), string, 0)
-
-    return ct in range(min, max+1)
-
+    return reduce(ct_reduce(char), string, 0) in range(min, max+1)
 
 ###########################
 # Part 2: Check positions #
 ###########################
 
 
-def poscheck_reduce(char: str, pos1: int, pos2: int) -> Callable[[bool, str, int], bool]:
-    def red(acc: bool, cur: str, idx: int) -> bool:
-        # XOR (^) because exactly one position has to match
-        return acc ^ True if cur == char and idx in (pos1, pos2) else acc
-
-    return red
-
-
 def pos_check(parsed: ParsedLine) -> bool:
-    ((pos1, pos2), char, string) = parsed
+    (positions, char, string) = parsed
 
-    return indexed_reduce(poscheck_reduce(char, pos1-1, pos2-1), list(string), False)
-
+    return reduce(lambda x, y: x ^ y, [string[pos-1] == char for pos in positions])
 
 #####################
 # Do the damn thing #
@@ -106,7 +63,7 @@ def check_lines(lines: List[str]) -> Tuple[int, int]:
     return (len(accepted1), len(accepted2))
 
 
-print(check_lines(example))
-
 with open('i02.txt', 'r') as f:
+    print('EXPECTED:\n(2,1)\n(536,558)\n\nACTUAL:')
+    print(check_lines(example))
     print(check_lines(f.readlines()))
